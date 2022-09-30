5f5c8d5ec32bba6486c1e7c2fd2d2168044ba0fe
Sanjeev Kumar <sanjeev.kumar@broadcom.com>
2022-09-29 23:21:45 -0700

    TF-IDF merging

--- src/main/java/io/anserini/rerank/lib/BM25SynonymReranker.java
+++ src/main/java/io/anserini/rerank/lib/BM25SynonymReranker.java
@@ -135,7 +135,6 @@ public class BM25SynonymReranker implements Reranker {
     stringFloatMap.entrySet()
         .stream()
         .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-        .limit( 50 )
         .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
 
   //  System.out.println("reverseSortedMap>>>>"+reverseSortedMap);
